26353ab2fb7e35c6ca0dc1acef1c4d07256fada8
EufranCR <eufanzky@gmail.com>
2021-08-13 12:36:44 -0400

    Añadi el CSS y el descuento con cupo al taller 2

--- Taller2/descuentos.js
+++ Taller2/descuentos.js
@@ -1,4 +1,9 @@
-function calcularPrecioConDescuento (precio, descuento, cupon) {
+function calcularPrecioConDescuento (precio, descuento) {
+    const porcentajePrecioConDescuento = 100 - descuento; 
+    const precioFinal = (precio * porcentajePrecioConDescuento) / 100;
+    return precioFinal;
+}
+function calcularPrecioConDescuentoYCupon(precio, descuento, cupon){
     const porcentajePrecioConDescuento = 100 - descuento - cupon; 
     const precioFinal = (precio * porcentajePrecioConDescuento) / 100;
     return precioFinal;
@@ -14,8 +19,12 @@ function mostrarDescuento() {
     const discountValue = inputDiscount.value;
     const couponValue = inputCoupon.value;
 
-    const precioconDescuento = calcularPrecioConDescuento(priceValue, discountValue, couponValue);
+    const precioconDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+    const precioconDescuentoYCupon = calcularPrecioConDescuentoYCupon(priceValue, discountValue, couponValue)
+    
+    const resultP1 = document.getElementById("ResultP1");
+    resultP1.innerText = "El precio con descuento sin cupon es: $" + precioconDescuento;
 
-    const resultP = document.getElementById("ResultP");
-    resultP.innerText = "El precio con descuento son: $" + precioconDescuento;
+    const resultP2 = document.getElementById("ResultP2");
+    resultP2.innerHTML = "Elprecio con descuento y el cupon es: $" + precioconDescuentoYCupon;
 }
